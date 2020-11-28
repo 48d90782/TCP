3a831e0c6e401369dd383d717a8c85eb2e1ab281
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-28 20:01:34 +0300

    Revc to tun

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::process::Command;
+use tun_tap::{Iface, Mode};
+use std::io;
 
-fn main() {
+fn main() -> io::Result<()> {
+    let iface = Iface::new("tun0", Mode::Tun)?;
+    eprintln!("Iface {:?}", iface.name());
+    let name = iface.name();
+    let mut buf = vec![0; 1504]; // 1500 + 4 bytes header
 
+    iface.recv(&mut buf)?;
+    Ok(())
 }
 
 fn cmd(cmd: &str, args: &[&str]) {
     let ecode = Command::new("ip")
-        .arg(args)
+        .args(args)
         .spawn()
         .unwrap()
         .wait()
         .unwrap();
-    assert!(ecode.code(), "Failed to execute {}", cmd);
+    println!("Command: {}, exit code: {}", cmd, ecode.code().unwrap());
 }
