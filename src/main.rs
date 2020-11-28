2faac50f9d9c2fb42284f68dd46c6777e26efa96
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-28 19:02:30 +0300

    IP command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+
+}
+
+fn cmd(cmd: &str, args: &[&str]) {
+    let ecode = Command::new("ip")
+        .arg(args)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+    assert!(ecode.code(), "Failed to execute {}", cmd);
 }
