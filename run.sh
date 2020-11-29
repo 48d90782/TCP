b933a0c705411e0917e01376920f96fb7408530a
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-29 13:48:46 +0300

    Add flags and proto unpacking

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
 cargo b --release
 sudo setcap cap_net_admin=eip target/release/tcp
 
 ./target/release/tcp &
 pid=$!
-sudo ip addr add dev tun0 192.168.34.1/24
+sudo ip addr add dev tun0 10.10.10.1/24
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
 
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use std::process::Command;
 use tun_tap::{Iface, Mode};
 use std::io;
 
+///  3.2 Frame format:
+//   If flag IFF_NO_PI is not set each frame format is:
+//      Flags [2 bytes]
+//      Proto [2 bytes]
+//      Raw protocol(IP, IPv6, etc) frame.
 fn main() -> io::Result<()> {
+    // https://en.wikipedia.org/wiki/TUN/TAP
     let iface = Iface::new("tun0", Mode::Tun)?;
     println!("Iface name: {}", iface.name());
     let mut buf = [0; 1504]; // 1500 + 4 bytes header
@@ -10,6 +16,13 @@ fn main() -> io::Result<()> {
     loop {
         let nbytes = iface.recv(&mut buf)?;
         println!("bytes read: {}", nbytes);
+
+
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        println!("flags: {}", flags);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        println!("proto: {}", proto);
+
         println!("data: {:?}", &buf[..=nbytes]);
     }
 
